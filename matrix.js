35aeafd12c1381ef75b56dfe860764e1070a9b43
Yamato61 <jeshuaabrahamd@gmail.com>
2026-03-11 14:15:33 -0600

    Added comments

--- matrix.js
+++ matrix.js
@@ -7,9 +7,11 @@
 // [1,0] [1,1]
 
 function printMatrix(n) {
+  //for loop to iterate through the rows and columns of the matrix
   for (let i = 0; i < n; i++) {
     let row = "";
     for (let j = 0; j < n; j++) {
+      //add the current position to the row string
       row += "[" + i + "," + j + "] ";
     }
     console.log(row);
